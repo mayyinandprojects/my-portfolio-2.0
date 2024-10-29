bbbf598eab52cb98d65d91c0dea8c2bba4f3dd3f
may_ <lammayyin@gmail.com>
2024-10-30 00:33:10 +0100

    update config for vite for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  base: '/', // Ensures the correct path for assets
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+});
